20abedd4f41ca96fd26520614f3b64829616305e
jonatastalves <81393641+jonatastalves@users.noreply.github.com>
2022-04-18 13:43:13 -0300

    :sparkles: feat: inclusao api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:''
-})
+    baseURL:'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccosmos%2Cdacxi%2Cterra-luna&vs_currencies=usd%2Cbrl%2Ceur'
+});
+
+export default api;
+
